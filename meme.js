aae2f82cdddd0fd5da6eee30b7d611c3386ec655
Robbo00 <rscrog606@west-mec.org>
2025-08-12 15:21:02 -0700

    ertyuiop[

---
+++ meme.js
@@ -0,0 +1,6 @@
+let pics = ['https://shorturl.at/JWiYH', 'https://shorturl.at/R38Ng']
+let quotes = [
+    'I dont even say bless you when people sneeze anymore I just stare at em like this', 
+    'When somebody tells you a secret you already knew so yiu gotta act suprised',
+    
+]
