7ab2aa809720d4d568f6b8acdcc7dca372c1a412
Robbo00 <rscrog606@west-mec.org>
2025-08-13 15:22:56 -0700

    .

--- meme.js
+++ meme.js
@@ -1,6 +1,15 @@
-let pics = ['https://shorturl.at/JWiYH', 'https://shorturl.at/R38Ng']
+let pics = ['https://tinyurl.com/2js43e3e', 'https://shorturl.at/R38Ng']
 let quotes = [
     'I dont even say bless you when people sneeze anymore I just stare at em like this', 
-    'When somebody tells you a secret you already knew so yiu gotta act suprised',
-    
+    'When somebody tells you a secret you already knew so you gotta act suprised',    
 ]
+let dynamic = document.getElementById('dynam')
+
+
+function test(id){
+    if(/(.jpg, .png, .gif, .jpeg)$/i.test(document.getElementById(id).innerHTML)){
+        console.log('ran')
+        let src = document.getElementById(id).innerHTML
+        dynamic.setAttribute('src', src)
+    }
+}
