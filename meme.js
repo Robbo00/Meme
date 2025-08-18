e3c87b236872214292759d258be15d4b25ef9867
Robbo00 <rscrog606@west-mec.org>
2025-08-18 15:19:33 -0700

    v

--- meme.js
+++ meme.js
@@ -1,5 +1,12 @@
-let pics = ['https://tinyurl.com/2js43e3e', 'https://shorturl.at/R38Ng']
-let quotes = [
+let pics = []
+let quotes = []
+try{
+    let pics = localStorage.getItem('pics')
+    let quotes = localStorage.getItem('quotes')
+}
+catch(error){
+    let pics = ['https://tinyurl.com/2js43e3e', 'https://shorturl.at/R38Ng']
+    let quotes = [
     'I dont even say bless you when people sneeze anymore I just stare at em like this', 
     'When somebody tells you a secret you already knew so you gotta act suprised',
     'rando',
@@ -8,9 +15,13 @@ let quotes = [
     'mandy',
     'candy'    
 ]
+}
+
 let dynamic = document.getElementById('dynam')
 let rand = document.getElementById('in')
 let match = document.getElementById('game')
+let form = document.getElementById('form')
+let img = document.getElementById('dynamImg')
 let a = '';  let b = '';  let c = '';    let d = ''
 let random = []
 
@@ -33,3 +44,34 @@ function red(){
 
 }
 
+function move(id){
+    match.style.bottom = '100%'
+    form.style.top = '5%'
+    if(id == 'back'){
+    match.style.bottom = '5%'
+    form.style.top = '100%'
+    }
+}
+
+function preview(id){
+    let cont = document.getElementById(id).value
+    img.innerHTML = cont
+}
+
+function test(id){
+    let url = document.getElementById(id).value
+    if(/(.url|.jpg|.jpeg)$/.test(url)){
+        img.style.backgroundImage = `url(${url})`
+    }
+}
+
+function upload(){
+    let url = document.getElementById('x').value
+    let quote = document.getElementById('quotePrev').value
+    pics.push(url)
+    quotes.push(quote)
+}
+function save(){
+    localStorage.setItem('pics', pics)
+    localStorage.setItem('quotes', quotes)
+}
